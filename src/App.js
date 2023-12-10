6b1baaa6b4c46df4be995750cb78fa6a91366f60
CaptainBeatty <juliengrange.dev@gmail.com>
2023-12-10 16:09:15 +0100

    a routine test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import wojak from './wojak.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={wojak} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
